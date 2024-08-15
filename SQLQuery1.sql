c035bba78b4e8e268edfa7c9abe5f004d684d55a
Amit Patil <ampatilofficial49@gmail.com>
2024-08-15 23:43:34 +0530

    Add project files.

---
+++ SQLQuery1.sql
@@ -0,0 +1,34 @@
+
+/*
+If we want to delete specific data  -  Delete command
+Key facts
+1. DML operation
+2. If where condition is not specified, it deletes the whole data from table.
+
+command
+delete from EmployeeTwo;
+*/
+
+/*
+delete command is not prefered sometimes, if we want to delete the whole data from table.
+Command - Truncate
+
+Key Facts
+1. DDL command
+
+Truncate Table EmployeeTwo;
+*/
+
+
+/*
+If we want to delete the whole data along with table structure
+Command
+Drop
+
+Key Facts
+1. Delete the whole table structure
+2. DDL command
+
+Drop Table EmployeeTwo;
+*/
+
